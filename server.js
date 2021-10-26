499a8020c965c54e5690cf07eddd73c68c9b7221
mr-mohit <44066175+mr-mohit@users.noreply.github.com>
2021-10-26 06:16:54 -0400

    port number big fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app_1 = __importDefault(require("./server/config/app"));
 const debug_1 = __importDefault(require("debug"));
 debug_1.default('assignment1:server');
 const http_1 = __importDefault(require("http"));
-let port = normalizePort(process.env.PORT || '3000');
+let port = normalizePort(process.env.port || '3000');
 app_1.default.set('port', port);
 let server = http_1.default.createServer(app_1.default);
 server.listen(port);
